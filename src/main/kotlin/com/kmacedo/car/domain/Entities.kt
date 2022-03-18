373d1f0a4051fcd1562bd9dda05c144e1e04a972
cskmacedo <kleber.barreto@msn.com>
2022-03-18 00:49:17 -0300

    Primeiro commit

---
+++ src/main/kotlin/com/kmacedo/car/domain/Entities.kt
@@ -0,0 +1,14 @@
+package com.kmacedo.car.domain
+
+import java.time.LocalDate
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class Driver(
+
+    @Id
+    var id: Long? = null,
+    val name: String,
+    val birthDate: LocalDate
+)
---
+++ src/main/kotlin/com/kmacedo/car/domain/Repositories.kt
@@ -0,0 +1,5 @@
+package com.kmacedo.car.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface DriverRepository : JpaRepository<Driver, Long>
