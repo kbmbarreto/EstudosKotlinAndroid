bf874bf077ebf1a6c8a7064afaa5e87d829d97a6
cskmacedo <kleber.barreto@msn.com>
2022-04-11 23:18:26 -0300

    Condicionais em Kotlin

---
+++ KotlinBasico/src/main/kotlin/ControleFluxoWHEN.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var menu = 1
+
+    when(menu) {
+        1 -> println("Para inserir créditos, digite 1")
+        2 -> println("Mudança de Planos, digite 2")
+        3 -> println("Tim +25 e Tim +5, 3")
+        4 -> println("Falar com atendente, 4")
+        else -> println("Opção inválida")
+    }
+}
