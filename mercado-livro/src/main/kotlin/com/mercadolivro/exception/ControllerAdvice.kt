a4b6ae0fc5d1c78fb53d7daa6725147847d619cc
cskmacedo <kleber.barreto@msn.com>
2022-05-24 09:53:48 -0300

    Finalizada a aula 57

---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/exception/ControllerAdvice.kt
@@ -0,0 +1,24 @@
+package com.mercadolivro.exception
+
+import com.mercadolivro.controller.response.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+class ControllerAdvice {
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(ex: Exception, request: WebRequest): ResponseEntity<ErrorResponse> {
+
+        val erro = ErrorResponse(
+            httpCode = 400,
+            message = "Este recurso não existe",
+            internalCode = "0001",
+            errors = null
+        )
+
+        return ResponseEntity(erro, HttpStatus.BAD_REQUEST)
+    }
+}
