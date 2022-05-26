315a532973f36927ea581595c46ebc54cdc7f1c8
cskmacedo <kleber.barreto@msn.com>
2022-05-26 03:01:08 -0300

    Terminada a aula 63

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostBookRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostBookRequest.kt
@@ -1,11 +1,17 @@
 package com.mercadolivro.controller.request
 
 import com.fasterxml.jackson.annotation.JsonAlias
+import jdk.jfr.Name
 import java.math.BigDecimal
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 data class PostBookRequest (
+
+    @field:NotEmpty(message = "Nome deve ser informado")
     var name: String,
 
+    @field:NotNull(message = "Preço deve ser informado")
     var price: BigDecimal,
 
     @JsonAlias("customer_id")
--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
@@ -3,6 +3,8 @@ package com.mercadolivro.controller.request
 import java.math.BigDecimal
 
 data class PutBookRequest(
+
+
     var name: String?,
     var price: BigDecimal?
 )
